65736ffd6d2bdc53f5a028ae8c32570839ac7766
tefh2908 <00001105614918sp@al.educacao.sp.gov.br>
2024-10-08 18:47:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função',
+    'uma função é um bloco de código que executa '
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês',
+    'Oi em Inglês é HI (RAI)'
+)
+
+/*criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)*/
